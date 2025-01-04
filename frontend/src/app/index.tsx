82e2c62513b25832bca9181295e7d2da4f95ba74
overzlo <flashzed_969@mail.ru>
2025-01-05 00:23:07 +0500

    trending-carouser/test

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import { RegisterRoutes } from '@/modules/register/model/routes'
+import { Trending } from '../pages/trend/ui/Trending';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<></>} />
       <Route {...RegisterRoutes} />
+      <Route path ="/explore/trending" element = {<Trending/>} />
     </Routes>
   )
 }
---
+++ frontend/src/pages/trend/ui/Trending.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import { TrendingCarousel } from '@/widgets/trending-carousel';
+
+export const Trending = () => {
+    return (
+        <TrendingCarousel/>
+    );
+}
